5ffd4352e74202994aaf70c1814c89ce88690731
jlts <jlts@gft.com>
2023-05-12 13:26:42 -0300

    theme navbar

--- src/pages/components/AppBar.tsx
+++ src/pages/components/AppBar.tsx
@@ -1,45 +1,46 @@
-import * as React from 'react';
-import { useRouter } from 'next/router';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import Menu from '@mui/material/Menu';
-import MenuIcon from '@mui/icons-material/Menu';
-import Container from '@mui/material/Container';
-import Button from '@mui/material/Button';
-import MenuItem from '@mui/material/MenuItem';
-import Face3Icon from '@mui/icons-material/Face3';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-import Link from 'next/link';
+import * as React from "react";
+import { useRouter } from "next/router";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import Menu from "@mui/material/Menu";
+import MenuIcon from "@mui/icons-material/Menu";
+import Container from "@mui/material/Container";
+import Button from "@mui/material/Button";
+import MenuItem from "@mui/material/MenuItem";
+import Face3Icon from "@mui/icons-material/Face3";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import Link from "next/link";
 
-const pages = ['Início', 'Histórico'];
+const pages = ["Início", "Histórico"];
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#6b21a8",
+    },
+  },
+  components: {
+    MuiMenuItem: {
+      styleOverrides: {
+        root: {
+          "&:hover": {
+            backgroundColor: "#a855f7",
+            color: "white",
+          },
+        },
+      },
+    },
+  },
+});
 
 function ResponsiveAppBar() {
-    const router = useRouter();
-    const theme = createTheme({
-        palette: {
-            primary: {
-                main: '#6b21a8'
-            }
-        },
-        components: {
-          MuiMenuItem: {
-            styleOverrides: {
-              root: {
-               "&:hover": {
-                backgroundColor: '#a855f7',
-                color: 'white'
-               }
-              }
-            }
-          }
-        }
-    })
-
-
-  const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(null);
+  const router = useRouter();
+  const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(
+    null
+  );
 
   const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
     setAnchorElNav(event.currentTarget);
@@ -48,103 +49,116 @@ function ResponsiveAppBar() {
     setAnchorElNav(null);
   };
 
-
   return (
-    <ThemeProvider theme={theme}> 
-    <AppBar position="static" color='primary'>
-      <Container maxWidth="xl">
-        <Toolbar disableGutters>
-          <Face3Icon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }}  />
-          <Typography
-            variant="h6"
-            noWrap
-            component="a"
-            sx={{
-              mr: 2,
-              display: { xs: 'none', md: 'flex' },
-              fontFamily: 'monospace',
-              fontWeight: 700,
-              letterSpacing: '.3rem',
-              color: 'inherit',
-              textDecoration: 'none',
-            }}
-          >
-            LULU
-          </Typography>
-
-          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
-            <IconButton
-              size="large"
-              aria-label="account of current user"
-              aria-controls="menu-appbar"
-              aria-haspopup="true"
-              onClick={handleOpenNavMenu}
-              color="inherit"
-            >
-              <MenuIcon />
-            </IconButton>
-            <Menu
-              id="menu-appbar"
-              anchorEl={anchorElNav}
-              anchorOrigin={{
-                vertical: 'bottom',
-                horizontal: 'left',
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'left',
-              }}
-              open={Boolean(anchorElNav)}
-              onClose={handleCloseNavMenu}
+    <ThemeProvider theme={theme}>
+      <AppBar position="static" color="primary">
+        <Container maxWidth="xl">
+          <Toolbar disableGutters>
+            <Face3Icon sx={{ display: { xs: "none", md: "flex" }, mr: 1 }} />
+            <Typography
+              variant="h6"
+              noWrap
+              component="a"
               sx={{
-                display: { xs: 'block', md: 'none' },
+                mr: 2,
+                display: { xs: "none", md: "flex" },
+                fontFamily: "monospace",
+                fontWeight: 700,
+                letterSpacing: ".3rem",
+                color: "inherit",
+                textDecoration: "none",
               }}
             >
-              {pages.map((page) => (
-                <MenuItem key={page} onClick={handleCloseNavMenu} className="hover:bg-purple-500">
-                            <Link href={page === "Início" ? "/" : "/historic"} key={page}>
-                  <Typography textAlign="center">{page}</Typography>
-                  </Link>
-                </MenuItem>
-              ))}
-            </Menu>
-          </Box>
-          <Face3Icon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} />
-          <Typography
-            variant="h5"
-            noWrap
-            component="a"
-            sx={{
-              mr: 2,
-              display: { xs: 'flex', md: 'none' },
-              flexGrow: 1,
-              fontFamily: 'monospace',
-              fontWeight: 700,
-              letterSpacing: '.3rem',
-              color: 'inherit',
-              textDecoration: 'none',
-            }}
-          >
-            LULU
-          </Typography>
-          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
-            {pages.map((page) => (
-                      <Link href={page === "Início" ? "/" : "/historic"} key={page}>
-              <Button
-                key={page}
-                onClick={handleCloseNavMenu}
-                className={`hover:bg-purple-500 ${router.pathname === (page === "Início" ? "/" : "/historic") ? "bg-purple-500" : ""}`}
-                sx={{ my: 2, color: 'white', display: 'block', m: 1 }}
+              LULU
+            </Typography>
+
+            <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
+              <IconButton
+                size="large"
+                aria-label="account of current user"
+                aria-controls="menu-appbar"
+                aria-haspopup="true"
+                onClick={handleOpenNavMenu}
+                color="inherit"
               >
-                {page}
-              </Button>
-              </Link>
-            ))}
-          </Box>
-        </Toolbar>
-      </Container>
-    </AppBar>
+                <MenuIcon />
+              </IconButton>
+              <Menu
+                id="menu-appbar"
+                anchorEl={anchorElNav}
+                anchorOrigin={{
+                  vertical: "bottom",
+                  horizontal: "left",
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: "top",
+                  horizontal: "left",
+                }}
+                open={Boolean(anchorElNav)}
+                onClose={handleCloseNavMenu}
+                sx={{
+                  display: { xs: "block", md: "none" },
+                }}
+              >
+                {pages.map((page) => (
+                  <MenuItem
+                    key={page}
+                    onClick={handleCloseNavMenu}
+                    sx={{
+                      "&:hover": { backgroundColor: "#a855f7", color: "white" },
+                    }}
+                  >
+                    <Link
+                      href={page === "Início" ? "/" : "/historic"}
+                      key={page}
+                    >
+                      <Typography textAlign="center">{page}</Typography>
+                    </Link>
+                  </MenuItem>
+                ))}
+              </Menu>
+            </Box>
+            <Face3Icon sx={{ display: { xs: "flex", md: "none" }, mr: 1 }} />
+            <Typography
+              variant="h5"
+              noWrap
+              component="a"
+              sx={{
+                mr: 2,
+                display: { xs: "flex", md: "none" },
+                flexGrow: 1,
+                fontFamily: "monospace",
+                fontWeight: 700,
+                letterSpacing: ".3rem",
+                color: "inherit",
+                textDecoration: "none",
+              }}
+            >
+              LULU
+            </Typography>
+            <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
+              {pages.map((page) => (
+                <Link href={page === "Início" ? "/" : "/historic"} key={page}>
+                  <Button
+                    key={page}
+                    onClick={handleCloseNavMenu}
+                    className={`hover:bg-purple-500 ${
+                      router.pathname ===
+                      (page === "Início" ? "/" : "/historic")
+                        ? "bg-purple-500"
+                        : ""
+                    }`}
+                    sx={{ my: 2, color: "white", display: "block", m: 1 }}
+                  >
+                    {page}
+                  </Button>
+                </Link>
+              ))}
+            </Box>
+          </Toolbar>
+        </Container>
+      </AppBar>
     </ThemeProvider>
   );
 }
